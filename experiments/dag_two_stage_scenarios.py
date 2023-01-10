6d4e7f76b55628ba48a8eeebf8aaee212b9e4d85
fonhorst <fonhorst@alipoov.nb@gmail.com>
2023-01-10 22:41:44 +0300

    added settings to run workflow submit

--- experiments/dag_two_stage_scenarios.py
+++ experiments/dag_two_stage_scenarios.py
@@ -413,10 +413,10 @@ ml20m_first_level_dag = build_fit_predict_first_level_models_dag(
     dataset=DATASETS["ml20m"]
 )
 
-ml25m_first_level_dag = build_fit_predict_first_level_models_dag(
+ml25m_first_level_dag_submit = build_fit_predict_first_level_models_dag(
     dag_id="ml25m_first_level_dag",
     mlflow_exp_id="111",
-    model_params_map=_get_models_params("als", "itemknn", "ucb", "slim"),
+    model_params_map=_get_models_params("als"), #, "itemknn", "ucb", "slim"),
     dataset=DATASETS["ml25m"]
 )
 
